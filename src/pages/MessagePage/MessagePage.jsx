a2fac30e51d8e866525adb0fde9266aef901cb0d
andrewhuang192 <andrewhuang192@gmail.com>
2021-05-07 03:04:13 -0700

    mvp for demo

--- src/pages/MessagePage/MessagePage.jsx
+++ src/pages/MessagePage/MessagePage.jsx
@@ -188,17 +188,7 @@ export default function MessagePage({ user, users, handleAddMessage }) {
               <ol className={classes.ol}>
 			  
 			  {messageItems.filter((message) => message.conversation === activeConversation).map((message, i)  => (
-                  //   <li
-                  //     key={i}
-                  //     className="messageItem"
-                  //     // className={clsx(classes.message, message.isOwner ? classes.owner : classes.guest)}
-                  //   >
-                  //     <span className="textmessage">{message.message}</span>
-                  //     <span className="timestamp">{message.createdAt}</span>
-                  //     <span className="name">
-                  //       {message.isOwner ? `${user.name}` : "friend"}
-                  //     </span>
-                  //   </li>
+               
                   <li
                     key={message._id}
                     className={clsx(
