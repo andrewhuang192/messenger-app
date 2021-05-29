d461cf8d8f896e87dedc44bfe2f27d5728bc1edb
andrewhuang192 <andrewhuang192@gmail.com>
2021-05-29 15:40:48 -0700

    authpage fixes

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import {useState} from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-import SignUpForm from '../../Components/SignUpForm/SignUpForm';
-import LoginForm from '../../Components/LoginForm/LoginForm';
-import ForgotPassword from '../../Components/ForgotPassword/ForgotPassword';
+import SignUpForm from "../../Components/SignUpForm/SignUpForm";
+import LoginForm from "../../Components/LoginForm/LoginForm";
+import ForgotPassword from "../../Components/ForgotPassword/ForgotPassword";
 
-import './AuthPage.css';
+import "./AuthPage.css";
 
 export default function AuthPage({ setUser }) {
-	const [showLogin, setShowLogin] = useState(true);
-	// const [showForgotPassword, setShowForgotPassword] = useState()
-
-	return (
-		<main>
-			<h1>AuthPage</h1>
-			<div><ForgotPassword /></div>
-			{showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
-			<div>
-				<Link to='/' onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'Create an Account' : 'Already have an account?'}</Link>
-			</div>
-			{/* &nbsp; | &nbsp; */}
-		</main>
-	);
+  const [showLogin, setShowLogin] = useState(true);
+  const [showForgotPassword, setShowForgotPassword] = useState(false);
+  console.log(showForgotPassword);
+  return (
+    <main>
+      <h1>AuthPage</h1>
+      {showForgotPassword ? (
+        <div>
+          
+        </div>
+      ) : (
+        ""
+      )}
+	  
+      {showLogin & !showForgotPassword ? (<LoginForm setUser={setUser} />) 
+	  	: !showLogin & showForgotPassword ? (<ForgotPassword />)
+		: !showLogin & !showForgotPassword ? (<SignUpForm setUser={setUser} />)
+		: null
+      }
+      <div>
+        <Link to="/" onClick={() => setShowLogin(!showLogin)}>
+          {showLogin ? "Create an Account" : "Already have an account?"}
+        </Link>
+        <br />
+        <Link to="/" onClick={() => setShowForgotPassword(!showForgotPassword)}>
+          {!showForgotPassword ? "Reset Password" : "Cancel"}
+        </Link>
+      </div>
+      {/* &nbsp; | &nbsp; */}
+    </main>
+  );
 }
